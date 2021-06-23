a1fa101e45ede28c392cc1f76099321cf4a43efc
Issak <aizekyuy@yandex.ru>
2021-06-23 18:22:15 +0600

    update AboutUs and added global sass styles

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,3 +7,19 @@ export {default as shopCart} from "../assests/btnImage/shopping-bag 1.svg";
 export {default as user} from "../assests/btnImage/user 1.svg";
 export {default as favorite} from "../assests/btnImage/Fovarite.svg";
 export {default as dropBtn} from "../assests/btnImage/dropVector.svg";
+
+//lock_nextImg
+export {default as arrowBrown} from "../assets/lock_nextImg/arrow_brown.png";
+export {default as lock} from "../assets/lock_nextImg/lock.png";
+export {default as next} from "../assets/lock_nextImg/next.png";
+
+//verticalSlider
+export {default as vertical1} from "../assets/verticalSlider/vertical1.png";
+export {default as vertical2} from "../assets/verticalSlider/vertical2.png";
+export {default as vertical3} from "../assets/verticalSlider/vertical3.png";
+export {default as vertical4} from "../assets/verticalSlider/vertical4.png";
+
+//sliderImage
+export {default as sliderImg1} from "../assets/sliderImg/01.jpg";
+export {default as sliderImg2} from "../assets/sliderImg/02.jpg";
+export {default as sliderImg3} from "../assets/sliderImg/03.jpg";
