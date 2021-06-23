6bb91e88d3c822e9d687681de2a665459ba9b298
nurbek bekmuratov <nurbekbekmuratov03@gmail.com>
2021-06-23 16:50:24 +0600

    img

--- src/assests/index.js
+++
@@ -1,9 +0,0 @@
-//logo
-export {default as logo} from "../assests/image/Bella.svg";
-
-//btnLinkMenu
-export {default as search} from "../assests/btnImage/search 1.svg";
-export {default as shopCart} from "../assests/btnImage/shopping-bag 1.svg";
-export {default as user} from "../assests/btnImage/user 1.svg";
-export {default as favorite} from "../assests/btnImage/Fovarite.svg";
-export {default as dropBtn} from "../assests/btnImage/dropVector.svg";
