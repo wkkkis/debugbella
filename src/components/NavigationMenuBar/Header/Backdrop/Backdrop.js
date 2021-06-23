d4dd776befb31d3d78b48ef9e930ec77fe970f8e
EmirlanDogdurbaev <jigit38@gmail.com>
2021-06-23 14:49:57 +0300

    commit number 2 by Emirlan

---
+++ src/components/NavigationMenuBar/Header/Backdrop/Backdrop.js
@@ -0,0 +1,10 @@
+  
+import classes from "./Backdrop.module.scss";
+
+const Backdrop = ({ show, click }) => {
+  return (
+    show ? <div className={classes.Backdrop} onClick={click}></div> : null
+  );
+}
+
+export default Backdrop;
