cd5bb91394d6d032c8f2d1f592ff5170e1bcc61c
EmirlanDogdurbaev <jigit38@gmail.com>
2021-06-30 15:25:11 +0300

    added search down

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import { Footer, Layout } from "./containers";
 import { Route, Switch } from "react-router-dom";
 import {
-    AboutUs,
-    Favorite,
-    Main,
-    NewsCart,
-    Products,
-    Delivery,
-    PageNotFound,
-    Favourites,
-    Category,
+  AboutUs,
+  Favorite,
+  Main,
+  NewsCart,
+  Products,
+  Delivery,
+  PageNotFound,
+  Favourites,
+  Category,
 } from "./Pages";
 import NewsDetails from "./components/NewsDetails/NewsDetails";
 
 function App() {
-    return (
-        <>
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Main} />
-                    <Route exact path="/about" component={AboutUs} />
-                    <Route exact path="/favourite" component={Favorite} />
-                    <Route exact path="/delivery" component={Delivery} />
-                    <Route exact path="/news" component={NewsCart} />
-                    <Route
-                        exact
-                        path="/news_details/"
-                        component={NewsDetails}
-                    />
-                    <Route exact path="/details" component={Products} />
-                    <Route exact path="/notFound" component={PageNotFound} />
-                    <Route exact path="/favourites" component={Favourites} />
-                    <Route exact path="/category" component={Category} />
-                </Switch>
-            </Layout>
-           {/*  <Footer /> */}
-
-        </>
-    );
+  return (
+    <>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={AboutUs} />
+          <Route exact path="/favourite" component={Favorite} />
+          <Route exact path="/delivery" component={Delivery} />
+          <Route exact path="/news" component={NewsCart} />
+          <Route exact path="/news_details/" component={NewsDetails} />
+          <Route exact path="/details" component={Products} />
+          <Route exact path="/notFound" component={PageNotFound} />
+          <Route exact path="/favourites" component={Favourites} />
+          <Route exact path="/category" component={Category} />
+        </Switch>
+      </Layout>
+      {/*  <Footer /> */}
+    </>
+  );
 }
 
 export default App;
--- src/containers/NavigationMenuBar/Header/Nav/Nav.jsx
+++ src/containers/NavigationMenuBar/Header/Nav/Nav.jsx
@@ -9,100 +9,154 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Nav = () => {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    let toggle = () => {
-        setVisible(!visible);
-    };
+  let toggle = () => {
+    setVisible(!visible);
+  };
 
-    return (
-        <div className={classes.Nav}>
-            <ul className={classes.ul}>
-                <div className={classes.left}>
-                    <NavItem url="/">Товары</NavItem>
-                    <NavItem url="/about">О нас</NavItem>
-                    <NavItem url="/news">Новости</NavItem>
-                    <NavItem url="/delivery">Доставка</NavItem>
-                    <NavItem url="/contacts"> Контакты</NavItem>
-                </div>
-                <NavItem url="/" exact>
-                    <img src={bella} alt="logo" />
-                </NavItem>
+  const [vid, setVid] = useState(false);
 
-                <div className={classes.right}>
-                    <NavItem url="/search">
-                        <span className={classes.img}>
-                            <img src={lupa} alt="logo" />
-                            Поиск
-                        </span>
-                    </NavItem>
-                    <NavItem url="/favourites">
-                        <span className={classes.img}>
-                            <img src={heart} alt="logo" />
-                            Избранные
-                        </span>
-                    </NavItem>
+  let toggle1 = () => {
+    setVid(!vid);
+  };
 
-                    <span
-                        style={{
-                            cursor: "pointer",
-                            display: "felx",
-                            alignItems: "center",
-                        }}
-                    >
-                        <div>
-                            <div
-                                className={classes.cont}
-                                style={{
-                                    position: "realtive",
-                                    marginRight: "0",
-                                }}
-                            >
-                                <img src={user} alt="logo" />
-                                <span
-                                    style={{ margin: "0" }}
-                                    onClick={toggle}
-                                    className={
-                                        visible
-                                            ? classes.hoverDropDown
-                                            : classes.hoverDropDown__color
-                                    }
-                                >
-                                    Профиль
-                                </span>
-                            </div>
-
-                            {visible && (
-                                <>
-                                    <div
-                                        className={classes.dropDown}
-                                        style={{ position: "absolute" }}
-                                    >
-                                        <span className={classes.link}>
-                                            <Link to="/orders">мои заказы</Link>
-                                        </span>
-                                        <span className={classes.link}>
-                                            <Link to="/profile">профиль</Link>
-                                        </span>
-                                        <span className={classes.link}>
-                                            <Link to="/notFound">выйти</Link>
-                                        </span>
-                                    </div>
-                                </>
-                            )}
-                        </div>
-                    </span>
-
-                    <NavItem url="/basket">
-                        <span className={classes.img}>
-                            <img src={shop} alt="logo" />
-                            Корзина
-                        </span>
-                    </NavItem>
-                </div>
-            </ul>
+  return (
+    <div className={classes.Nav}>
+      <ul className={classes.ul}>
+        <div className={classes.left}>
+          <NavItem url="/">Товары</NavItem>
+          <NavItem url="/about">О нас</NavItem>
+          <NavItem url="/news">Новости</NavItem>
+          <NavItem url="/delivery">Доставка</NavItem>
+          <NavItem url="/contacts"> Контакты</NavItem>
         </div>
-    );
+        <NavItem url="/" exact>
+          <img src={bella} alt="logo" />
+        </NavItem>
+
+        <div className={classes.right}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div
+              className={classes.cont}
+              style={{
+                position: "realtive",
+                marginRight: "0",
+              }}
+            >
+              <span
+                style={{ margin: "0" }}
+                onClick={toggle1}
+                className={
+                  vid ? classes.hoverSearch : classes.hoverSearchDown
+                }
+                style={{cursor:"pointer"}}
+              >
+                <img src={lupa} alt="logo" />
+                Поиск
+              </span>
+            </div>
+          </div>
+          {vid && (
+            <>
+              <div
+                className={classes.div1}
+
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    height: "44px",
+                  }}
+                >
+                  <input
+                    className={classes.input}
+                    type="text"
+                    placeholder="Искать"
+                  />
+                  <span>
+                    <button className={classes.btn1} onClick={toggle1}>
+                      <div className={classes.div2}>&times;</div>
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </>
+          )}
+
+          <NavItem url="/favourites">
+            <span className={classes.img}>
+              <img src={heart} alt="logo" />
+              Избранные
+            </span>
+          </NavItem>
+
+          <span
+            style={{
+              cursor: "pointer",
+              display: "felx",
+              alignItems: "center",
+            }}
+          >
+            <div>
+              <div
+                className={classes.cont}
+                style={{
+                  position: "realtive",
+                  marginRight: "0",
+                }}
+              >
+                <img src={user} alt="logo" />
+                <span
+                  style={{ margin: "0" }}
+                  onClick={toggle}
+                  className={
+                    visible
+                      ? classes.hoverDropDown
+                      : classes.hoverDropDown__color
+                  }
+                >
+                  Профиль
+                </span>
+              </div>
+
+              {visible && (
+                <>
+                  <div
+                    className={classes.dropDown}
+                    style={{ position: "absolute" }}
+                  >
+                    <span className={classes.link}>
+                      <Link to="/orders">мои заказы</Link>
+                    </span>
+                    <span className={classes.link}>
+                      <Link to="/profile">профиль</Link>
+                    </span>
+                    <span className={classes.link}>
+                      <Link to="/notFound">выйти</Link>
+                    </span>
+                  </div>
+                </>
+              )}
+            </div>
+          </span>
+
+          <NavItem url="/basket">
+            <span className={classes.img}>
+              <img src={shop} alt="logo" />
+              Корзина
+            </span>
+          </NavItem>
+        </div>
+      </ul>
+    </div>
+  );
 };
 
 export default Nav;
