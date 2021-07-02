dabe56aa05a16cb405e1de5f86277272837bb56f
Aizhan Alekbarova <alekbarova.aizhana@gmail.com>
2021-07-02 17:06:08 +0600

    aijan

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,11 +1,13 @@
 import { ActionTypes } from "../constants/action_types";
 
 const initialState = {
-
+    products: [],
+loading: false,
+error: null
 };
 
-export const productReducer = (state = initialState, {type, payload}) => {
-    switch(type){
+export const productReducer = (state = initialState, action) => {
+    switch(action.type){
         case :
         return {...state, payload}
         default: return state;
